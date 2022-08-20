667ba25dd5271584bc10a61325b15f1162e4ec83
oluwatomsin <70864196+oluwatomsin@users.noreply.github.com>
2022-08-20 14:56:31 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Distribution_1_3',
+    version='0.0.1',
+    description='Probability Distributions',
+    author='Tomi_Abdul',
+    packages=['Distribution'],
+    author_email='amusatomisin65@gmail.com',
+    zip_safe=False
+)
